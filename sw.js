12ce3853f8a14267d9d3aad8606d4c68e8719f9c
set-page <bobvchuck@gmail.com>
2025-10-15 17:01:25 +0800

    https://api.github.com/repos/set-page/home/contents/sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,10 @@
-self.addEventListener("push", async function (event) {
-    // Ambil data dari payload push
-    var payload = event.data ? event.data.json() : {};
-    var data = payload.data || payload; // handle FCM atau push langsung
-
-    // Pastikan title dan body ada
-    var title = data.title || "No Title";
-    var body = data.body || "No message content";
-
-    // Siapkan opsi notifikasi
-    var options = {
-        body: body,
-        data: {
-            url: data.url || "../home"
-        }
-    };
-
-    // Tampilkan notifikasi
-    event.waitUntil(
-        self.registration.showNotification(title, options)
-    );
+self.addEventListener('push', function(event) {
+  if (!event.data) return;
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      data: data.url
+    })
+  );
 });
